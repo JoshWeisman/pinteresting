6c6f1e87df779948b59e4223cd6d3c5780a8a9ed
Josh Weisman <josh@joshweismanfilm.com>
2016-08-24 02:17:45 -0700

    Added a menu bar to the top of each page, with working links to the other pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
 root "pages#home"
+  # NOTE: This creates a "root" path
+
 get "about" => "pages#about"
+  # NOTE: This creates an "about" path
+
 get "hello" => "pages#hello"
+  # NOTE: This creates a "hello" path
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
